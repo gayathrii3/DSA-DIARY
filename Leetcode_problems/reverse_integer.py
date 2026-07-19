7bbcdaf2c4ed28246c8510455428986191abd366
Gayathri Reddy <gayathrilakshmi344@gmail.com>
2026-07-19 20:08:25 +0530

    leetcode  #7
    Implemented a reverse function to reverse an integer while handling 32-bit overflow.

--- Leetcode_problems/reverse_integer.py
+++ Leetcode_problems/reverse_integer.py
@@ -1,9 +1,21 @@
-x = 123
-rev = 0
+# If reversing the number makes it to exceed the 32-bit integer range then the function should return 0
 
-while x > 0:
-    digit = x % 10
-    rev = rev * 10 + digit
-    x = x // 10
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x = abs(x)  #absolute value(changes -ve to +ve)
+        rev = 0
 
-print(rev)
+        while x:
+            digit = x % 10
+            rev = rev * 10 + digit
+            x = x // 10
+
+        rev = rev * sign
+
+        if rev < -(2 ** 31) or rev > 2 ** 31 - 1:
+            return 0
+
+        return rev
+    
+# TC = O(log10​ . N)
